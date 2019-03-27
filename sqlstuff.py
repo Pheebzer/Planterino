f1910ed9222519d30c107dcda1d0c59e51fd176c
pheebzer <omegalul>
2019-03-27 14:08:35 +0200

    Added prototype code for database connection/inserts

---
+++ sqlstuff.py
@@ -0,0 +1,25 @@
+import pymysql as pd
+import  datetime
+
+with open("creds.txt") as r:
+    lines  = r.readlines()
+    uname = lines[0]
+    passwd = lines[1]
+    host = lines[2]
+    r.close()
+
+uname = uname.rstrip()
+passwd = passwd.rstrip()
+host = host.rstrip()
+dbname = "moisture_info"
+date = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+holder = 20 #placeholder for register value
+stat = ("INSERT INTO moisturelog (date, moisture) VALUES (%s,%s)",(date,holder)) 
+
+conn = pd.connect(host, user=uname, port=3306, passwd=passwd, db=dbname)
+
+curs = conn.cursor()
+curs.execute(*stat)
+conn.commit()
+conn.close()
+
